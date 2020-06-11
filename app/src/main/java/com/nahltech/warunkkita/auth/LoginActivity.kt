22948146092093be2e6ea18309da8e4a5844b743
ddr137 <dadanramdhani1509@gmail.com>
2020-06-11 11:54:21 +0700

    add assets

--- app/src/main/java/com/nahltech/warunkkita/auth/LoginActivity.kt
+++ app/src/main/java/com/nahltech/warunkkita/auth/LoginActivity.kt
@@ -1,15 +1,38 @@
 package com.nahltech.warunkkita.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import com.nahltech.warunkkita.MainActivity
 import com.nahltech.warunkkita.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        clickIntent()
+        toolbarUI()
     }
 
-    fun onClick(view: View) {}
+    private fun clickIntent() {
+        register_now.setOnClickListener {
+            val moveIntent = Intent(this, RegisterActivity::class.java)
+            startActivity(moveIntent)
+        }
+        login.setOnClickListener {
+            val moveIntent = Intent(this, MainActivity::class.java)
+            startActivity(moveIntent)
+        }
+    }
+
+    private fun toolbarUI() {
+        setSupportActionBar(toolbar_login)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        toolbar_login.setNavigationIcon(R.drawable.ic_back)
+        toolbar_login.setNavigationOnClickListener { finish() }
+    }
 }
--- app/src/main/java/com/nahltech/warunkkita/auth/RegisterActivity.kt
+++ app/src/main/java/com/nahltech/warunkkita/auth/RegisterActivity.kt
@@ -1,12 +1,31 @@
 package com.nahltech.warunkkita.auth
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.nahltech.warunkkita.R
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        clickIntent()
+        toolbarUI()
+    }
+
+    private fun clickIntent() {
+        login_here.setOnClickListener {
+            val moveIntent = Intent(this, LoginActivity::class.java)
+            startActivity(moveIntent)
+            finish()
+        }
+    }
+
+    private fun toolbarUI() {
+        setSupportActionBar(toolbar_register)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        toolbar_register.setNavigationIcon(R.drawable.ic_back)
+        toolbar_register.setNavigationOnClickListener { finish() }
     }
 }
